31c4cceba906786e28119471b23508d85aff671c
GS04-49-WatanabeShintaro <shintan666@gmail.com>
2016-05-13 13:05:28 +0900

    フォームを改良した。定員が6名のため、dbに6名が登録されていると、予約フォームが表示されないようにした。

--- 7/wg/lesson.php
+++ 7/wg/lesson.php
@@ -20,6 +20,43 @@
 
 
 <body>
+
+<?php
+//DB接続
+$pdo = new PDO('mysql:dbname=wg_booked;charset=utf8;host=localhost', 'root', '');
+
+//DBからテーブルを取得
+$stmt = $pdo->prepare("SELECT id FROM wg_booked_table");
+$flag = $stmt->execute();
+
+//取得できたかチェック
+$view = "";
+$array = array();//配列を作る
+if($flag==false){ //$flag=falseが⼊っていればエラー
+  $view = "SQLエラー";
+} else {
+  //Selectデータの数だけ自動でループしてくれる
+  while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+  $array[] = $result['id']; //idを配列にいれていく
+  }
+}
+$countBooked = count($array); //予約者数
+
+?>
+
+<script>
+$(document).ready(function(){
+  var countArray = <?php echo json_encode($countBooked); ?>;
+  if(countArray >= 6){
+      $(".bookMain").addClass("text-center");
+      $(".bookMain").html("定員に達したため今回の募集は終了致しました。<br>次回のお申し込みをお待ちしております。<br>なお、募集開始のお知らせはInstagram上でもお知らせ致します。");
+  }
+});
+</script>
+
+
+
+
 <br>
 <p class="text-center bookTitle">
   <b>予約フォーム</b>
@@ -30,19 +67,19 @@
 <table align="center" width="500" cellspacing="5" cellpadding="5" >
   <tr>
     <td>お名前：</td>
-    <td><input type="text" name="name" size="36"></td>
+    <td><input type="text" name="name" size="36" required></td>
   </tr>
   <tr>
     <td>フリガナ：</td>
-    <td><input type="text" name="kana" size="36"></td>
+    <td><input type="text" name="kana" size="36" required></td>
   </tr>
   <tr>
     <td>メールアドレス：</td>
-    <td><input type="text" name="email" size="36"></td>
+    <td><input type="email" name="email" size="36" required></td>
   </tr>
   <tr>
     <td>電話番号：</td>
-    <td><input type="text" name="tel" size="36"></td>
+    <td><input type="tel" name="tel" size="36" required></td>
   </tr>
 </table>
 <br>
